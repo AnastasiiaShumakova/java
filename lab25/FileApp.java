b166be51015e859fff4196c8f96fc05311b8680a
AnastasiiaShumakova <nastya.shumakova.0505@mail.ru>
2024-12-23 21:20:48 +0400

    Add files via upload

---
+++ lab25/FileApp.java
@@ -0,0 +1,19 @@
+package org.knit.lab25;
+
+import java.util.Scanner;
+
+// Необходимо создать консольное приложение, которое принимает на вход путь к папке или файлу,
+// и сохраняет все файлы размером менее 10 MB в базу данных.
+// В базе данных создается таблица, в которой хранятся имена файлов и их содержимое в виде бинарных данных.
+
+public class FileApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь к файлу или папке: ");
+        String path = scanner.nextLine();
+
+        FileProcessor processor = new FileProcessor();
+        processor.processFiles(path);
+    }
+}
